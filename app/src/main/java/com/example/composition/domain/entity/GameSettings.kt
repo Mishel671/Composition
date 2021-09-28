5ca4cba82641e0517f0bddf28bb678651f2d5cd5
Mishel617 <Dzuba18071990@gmail.com>
2021-09-28 15:25:48 +0500

    Lottie anim

---
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,7 @@
+package com.example.composition.domain.entity
+
+data class GameSettings (
+    val maxSumValue: Int,
+    val minCountOfRightAnswer: Int,
+    val minPercentOfRightAnswer: Int,
+    val gameTimeInSeconds: Int)
